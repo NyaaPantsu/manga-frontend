43605f02d683449bf5b7e7271ce55f9f2f87de57
Eliot Whalan <ewhal@pantsu.cat>
2018-01-22 08:33:55 +1000

    more pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,62 @@
-
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'manga.sh',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Frontend for manga.sh, a Batoto replacement by the NyaaPantsu project.' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
-  },
-  plugins: [
-    '~/plugins/vuetify.js'
-  ],
-  modules: [
-    '@nuxtjs/axios'
-  ],
-  css: [
-    '~/assets/style/app.styl'
-  ],
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Axios configuration
-  */
-  axios: {
-    host: 'localhost',
-    port: 8080,
-    prefix: '/api/v1/',
-    baseURL: 'http://localhost:8080/api/v1/',
-    credentials: false
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    vendor: [
-      '~/plugins/vuetify.js'
-    ],
-    extractCSS: true,
     /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+     ** Headers of the page
+     */
+    head: {
+        title: 'manga.sh',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'Frontend for manga.sh, a Batoto replacement by the NyaaPantsu project.' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+        ]
+    },
+    plugins: [
+        '~/plugins/vuetify.js'
+    ],
+    modules: [
+        '@nuxtjs/axios'
+    ],
+    css: [
+        '~/assets/style/app.styl'
+    ],
+    /*
+     ** Customize the progress bar color
+     */
+    loading: { color: '#3B8070' },
+    /*
+     ** Axios configuration
+     */
+    axios: {
+        host: 'localhost',
+        port: 8080,
+        prefix: '/api/v1/',
+        baseURL: 'http://api.manga.sh:8080/api/v1/',
+        credentials: false
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        vendor: [
+            '~/plugins/vuetify.js'
+        ],
+        extractCSS: true,
+        /*
+         ** Run ESLint on save
+         */
+        extend(config, ctx) {
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
---
+++ pages/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout column justify-center align-center>
+  
+  
+    <v-form ref="form">
+      <v-text-field label="UserName" v-model="username" required></v-text-field>
+      <v-text-field label="Password" v-model="password" :type="'password'" required></v-text-field>
+  
+      <v-btn @click="submit">
+        submit
+      </v-btn>
+    </v-form>
+  
+  
+  </v-layout>
+</template>
+<script>
+export default {
+  name: 'login',
+  data: function () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submit: function (e) {
+      console.log(JSON.stringify({username: this.username, password: this.password}))
+      this.$axios.$post('/auth/login', JSON.stringify({username: this.username, password: this.password}))
+        .then(function (response) {
+
+        })
+    }
+  }
+}
+</script>
