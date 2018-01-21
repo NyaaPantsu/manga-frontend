fce22fc13c38e5ee28494e02ac811f6c7e845958
Anri <anri.tora@gmail.com>
2018-01-21 13:28:52 -0800

    Added a Login Component

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout>
+    <v-flex offset-xs4 xs4>
+      <v-card class="pa-4">
+        <div class="headline">Login</div>
+        <v-form v-model="valid">
+          <v-text-field
+            label="Name"
+            v-model="name"
+            :rules="nameRules"
+            :counter="10"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="E-mail"
+            v-model="email"
+            :rules="emailRules"
+            required
+          ></v-text-field>
+          <v-checkbox
+            label="Remember Me"
+            v-model="checkbox"
+          ></v-checkbox>
+          <div class="text-xs-center">
+            <v-btn
+              light
+              @click="submit"
+              :disabled="!valid"
+            >
+              submit
+            </v-btn>
+          </div>
+        </v-form>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
