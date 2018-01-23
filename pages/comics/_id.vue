9856a372b17c2a6fe85697c7bf8cdd21f1f2ac51
PantsuDev <ewhal@pantsu.cat>
2018-01-23 14:05:56 +1000

    More work

--- pages/comics/_id.vue
+++ pages/comics/_id.vue
@@ -1,12 +1,68 @@
 <template>
-    
+   <v-layout>
+    <v-flex xs12>
+      <v-card>
+        <v-card-media src="/static/doc-images/cards/desert.jpg" height="200px">
+        </v-card-media>
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn flat color="orange">Share</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+      <div md-5></div>
+      <v-data-table
+      v-bind:headers="headers"
+      :items="items"
+      hide-actions
+      class="elevation-1"
+    >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.name }}</td>
+      <td class="text-xs-right">{{ props.item.calories }}</td>
+      <td class="text-xs-right">{{ props.item.fat }}</td>
+      <td class="text-xs-right">{{ props.item.carbs }}</td>
+      <td class="text-xs-right">{{ props.item.protein }}</td>
+      <td class="text-xs-right">{{ props.item.sodium }}</td>
+      <td class="text-xs-right">{{ props.item.calcium }}</td>
+      <td class="text-xs-right">{{ props.item.iron }}</td>
+    </template>
+  </v-data-table>
+
+    </v-flex>
+  </v-layout>  
 </template>
 <script>
 export default {
   name: 'ComicsOne',
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          align: 'left',
+          sortable: false,
+          value: 'name'
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Sodium (mg)', value: 'sodium' },
+        { text: 'Calcium (%)', value: 'calcium' },
+        { text: 'Iron (%)', value: 'iron' }
+      ],
+      items: []
+    }
+  },
   async mounted () {
     const chapters = await this.$axios.$get('/series_chapters?limit=' + this.limit + '&offset=' + this.offset)
-    this.chapters = chapters
+    this.items = chapters
     console.log(chapters)
   }
 }
--- store/index.js
+++ store/index.js
@@ -1,9 +1,32 @@
-export const state = () => ({
-  sidebar: false
-})
+import Vuex from 'vuex'
 
-export const mutations = {
-  toggleSidebar (state) {
-    state.sidebar = !state.sidebar
-  }
+const store = () => {
+  return new Vuex.Store({
+    state: {
+      sidebar: false,
+      nsfw: false,
+      user: false,
+      username: null,
+      token: null
+    },
+    mutations: {
+      toggleSidebar (state) {
+        state.sidebar = !state.sidebar
+      },
+      toggleNsfw (state) {
+        state.nsfw = !state.nsfw
+      },
+      username (state, username) {
+        state.username = username
+      },
+      token (state, token) {
+        state.token = token
+      },
+      user (state) {
+        state.user = !state.user
+      }
+    }
+  })
 }
+
+export default store
