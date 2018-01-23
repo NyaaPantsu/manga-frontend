1c571314199406a60f0e4a9e672359090a5c7d79
Anri <anri.tora@gmail.com>
2018-01-23 00:23:21 -0800

    Front Page Component Updated with Latest Updates and Popular Series. A few image to accomadated them as well.

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,222 @@
 <template>
-  <v-layout column>
+  <v-layout row>
+    <v-flex xs12 md9>
+      <v-layout row>
+        <v-toolbar dark color="blue-grey darken-3" class="pa-0">
+          <v-toolbar-title class="white--text">Latest Updates</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>apps</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>list</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-layout>
 
-      <v-data-table
-      v-bind:headers="headers"
-      :items="chapters"
-      hide-actions
-      class="elevation-1"
-    >
-    <template slot="items" slot-scope="props">
-      <tr>
-        <td>
-          <router-link :to="{ name: 'comics-id', params: { id: props.item.SeriesId.Id }}">{{ props.item.SeriesId.Name }}</router-link>
-        </td>
-        <td>
-          
-          <router-link :to="{ name: 'reader-id', params: { id: props.item.Hash }}">Read</router-link>
-        </td>
-        <td><flag :iso="props.item.ChapterLanguage.Code" /></td>
-      </tr>
-    </template>
-  </v-data-table>
+      <v-layout class="pa-1">
+        <v-flex style="flex: 0 0 50px">
+        </v-flex>
+        <v-flex xs8 sm6 class="px-2">
+          Title
+        </v-flex>
+        <v-flex xs4 hidden-xs-only>
+          Group
+        </v-flex>
+        <v-flex xs4 sm2>
+          Date
+        </v-flex>
+      </v-layout>
+
+      <v-layout row class="px-2">
+        <v-flex style="flex: 0 0 50px"></v-flex>
+        <v-flex xs12>
+          <v-divider ></v-divider>
+        </v-flex>
+      </v-layout>
+      
+      <v-layout row class="px-2">
+        <v-flex style="flex: 0 0 50px">
+          <img src="/placeholder.jpg" />
+        </v-flex>
+        <v-flex xs12>
+          <v-layout class="pa-1">
+            <v-flex xs12>
+              <img style="float: left; padding: 3px;" src="/information.png"/> Scattered Courtesan
+            </v-flex>
+          </v-layout>
+          <v-divider ></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" /> Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex xs4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" />  Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex sm4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row class="px-2">
+        <v-flex style="flex: 0 0 50px">
+          <img src="/placeholder.jpg" />
+        </v-flex>
+        <v-flex xs12>
+          <v-layout class="pa-1">
+            <v-flex xs12>
+              <img style="float: left; padding: 3px;" src="/information.png"/> Scattered Courtesan
+            </v-flex>
+          </v-layout>
+          <v-divider ></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" /> Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex xs4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" />  Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex sm4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" />  Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex sm4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+
+
+        </v-flex>
+      </v-layout>
+
+      <v-layout row class="px-2">
+        <v-flex style="flex: 0 0 50px">
+          <img src="/placeholder.jpg" />
+        </v-flex>
+        <v-flex xs12>
+          <v-layout class="pa-1">
+            <v-flex xs12>
+              <img style="float: left; padding: 3px;" src="/information.png"/> Scattered Courtesan
+            </v-flex>
+          </v-layout>
+          <v-divider ></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" /> Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex xs4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+          <v-layout class="pa-1">
+            <v-flex xs8 sm6>
+              <img style="float: left; padding: 3px 4px;" src="/book_open.png" />  Vol.1 Ch.2: 2
+            </v-flex>
+            <v-flex sm4 hidden-xs-only>
+              Manga Cosmo0000000000
+            </v-flex>
+            <v-flex xs4 sm2>
+              A day ago
+            </v-flex>
+          </v-layout>
+          <v-divider></v-divider>
+        </v-flex>
+      </v-layout>
+
+    </v-flex>
+
+
+    <v-flex hidden-sm-and-down md3 class="pl-3">
+        <v-subheader>Popular Series</v-subheader>
+        <v-card color="blue-grey darken-3" class="white--text mb-3">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex style="flex: 0 0 50px">
+                <img src="/placeholder.jpg" />
+              </v-flex>
+              <v-flex xs11>
+                <div>
+                  <div>Ch.722: The Old You</div>
+                  <div>Tomo-chan wa Onna no ko!</div>
+                  <div>Dec 17</div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+        <v-card color="blue-grey darken-3" class="white--text mb-3">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex style="flex: 0 0 50px">
+                <img src="/placeholder.jpg" />
+              </v-flex>
+              <v-flex xs11>
+                <div>
+                  <div>Ch.722: The Old You</div>
+                  <div>Tomo-chan wa Onna no ko!</div>
+                  <div>Dec 17</div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+        <v-card color="blue-grey darken-3" class="white--text mb-3">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex style="flex: 0 0 50px">
+                <img src="/placeholder.jpg" />
+              </v-flex>
+              <v-flex xs11>
+                <div>
+                  <div>Ch.722: The Old You</div>
+                  <div>Tomo-chan wa Onna no ko!</div>
+                  <div>Dec 17</div>
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+    </v-flex>
 
   </v-layout>
 </template>
