ac2c648f410e661554d90b55cab1cd4b725685c5
Luna <luna@stor.lol>
2018-01-25 17:19:16 +1000

    add basic alert display

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,9 @@
   <v-app dark>
     <v-content>
       <v-container>
+        <template v-for="alert in $store.disptach('getAlerts')">
+          <v-alert :key="alert" :type="alert.type" :value="alert.text" />
+        </template>
         <nuxt />
       </v-container>
     </v-content>
