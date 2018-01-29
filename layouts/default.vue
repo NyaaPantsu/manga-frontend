61059a4cf6cca40667ae1cac61b71ef8e37e25cc
Luna <luna@stor.lol>
2018-01-29 20:47:48 +1000

    further work on fixing alerts

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <v-app dark>
     <v-content>
       <v-container>
-        <template v-for="alert in this.$store.state.getAlerts">
+        <template v-for="alert in this.$store.dispatch('alerts/getAlerts')">
           <v-alert :key="alert" :type="alert.type" :value="alert.text" />
         </template>
         <nuxt />
