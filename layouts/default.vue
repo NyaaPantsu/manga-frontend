e952f71bdadc0058fe693ef756451bc0ff3569bb
Luna <luna@stor.lol>
2018-01-29 21:14:02 +1000

    finish fixing alerts

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,38 @@
   <v-app dark>
     <v-content>
       <v-container>
-        <template v-for="alert in this.$store.dispatch('alerts/getAlerts')">
-          <v-alert :key="alert" :type="alert.type" :value="alert.text" />
+        <template v-for="alert in getAlerts">
+          <v-alert :key="alert" :type="alert.type" :value="alert.text">{{alert.text}}</v-alert>
         </template>
         <nuxt />
       </v-container>
     </v-content>
   </v-app>
 </template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+
+  computed: {
+    getAlerts () {
+      let alerts = []
+      for (let atype of ['success', 'warn', 'info', 'error']) {
+        for (let alertIndex in this.$store.state.alerts[atype]) {
+          if (typeof this.$store.state.alerts[atype][alertIndex] === 'object') {
+            alerts.push({
+              ...this.$store.state.alerts[atype][alertIndex],
+              type: atype
+            })
+          } else {
+            // error state
+          }
+          this.$store.dispatch('consumeAlert', { type: atype, index: alertIndex })
+        }
+      }
+      return alerts
+    }
+  }
+}
+</script>
+
--- store/alerts.js
+++ store/alerts.js
@@ -7,7 +7,20 @@ export const state = () => ({
 
 export const mutations = {
   addAlert (state, alert) {
-    state[alert.type].push(alert.alert)
+    if (typeof alert.alert === 'string') {
+      state[alert.type].push({
+        text: alert.alert,
+        remainingCount: 1,
+        type: alert.type
+      })
+    } else if (typeof alert.alert === 'object') {
+      state[alert.type].push({
+        ...alert.alert,
+        type: alert.type
+      })
+    } else {
+      // error state
+    }
   },
   removeAlert (state, alert) {
     state[alert.type] = state[alert.type].splice(alert.index, 1)
@@ -15,34 +28,32 @@ export const mutations = {
   clearAlert (state, type) {
     state[type] = []
   },
+  decrementAlertRemainingCount (state, alert) {
+    state[alert.type][alert.index].remainingCount -= 1
+  }
+}
+
+export const actions = {
   consumeAlert (store, alert) {
-    // error handling -- non-number type is error store.state
+    // error handling -- non-number type is error state
     if (typeof store.state[alert.type][alert.index].remainingCount !== 'number') {
       store.commit('removeAlert', alert)
+      return
     }
     // if not persistent alert
     if (store.state[alert.type][alert.index].remainingCount !== -1) {
-      store.state[alert.type][alert.index].remainingCount -= 1
+      store.commit('decrementAlertRemainingCount', { type: alert.type, index: alert.index })
       // if no remainingCount left, remove
       if (store.state[alert.type][alert.index].remainingCount === 0) {
         store.commit('removeAlert', alert)
       }
     }
-  }
-}
-
-export const actions = {
-  getAlerts (store) {
+  },
+  async getAlerts (store) {
     let alerts = []
     for (let atype of ['success', 'warn', 'info', 'error']) {
       for (let alertIndex in store.state[atype]) {
-        if (typeof store.state[atype][alertIndex] === 'string') {
-          alerts.push({
-            text: store.state[atype][alertIndex],
-            remainingCount: 1,
-            type: atype
-          })
-        } else if (typeof store.state[atype][alertIndex] === 'object') {
+        if (typeof store.state[atype][alertIndex] === 'object') {
           alerts.push({
             ...store.state[atype][alertIndex],
             type: atype
@@ -50,7 +61,7 @@ export const actions = {
         } else {
           // error state
         }
-        store.commit('consumeAlert', { type: atype, index: alertIndex })
+        store.dispatch('consumeAlert', { type: atype, index: alertIndex })
       }
     }
     return alerts
