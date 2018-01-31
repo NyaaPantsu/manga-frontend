94f0fd1378efc3da5192455fff1f031c4d1df454
PantsuDev <ewhal@pantsu.cat>
2018-01-31 13:26:14 +1000

    Add random + fix eslint errors

--- components/login.vue
+++ components/login.vue
@@ -53,9 +53,9 @@
               type: 'success',
               alert: 'You signed in successfully!'
             })
-            this.$cookie.set('token', data['token'], 1);
-            this.$cookie.set('user', true, 1);
-            this.$cookie.set('username', data['username'], 1);
+            this.$cookie.set('token', data['token'], 1)
+            this.$cookie.set('user', true, 1)
+            this.$cookie.set('username', data['username'], 1)
             this.$store.commit('username', data['username'])
             this.$store.commit('token', data['token'])
             this.$store.commit('user', true)
--- pages/comics/random.vue
+++ pages/comics/random.vue
@@ -1,7 +1,12 @@
 <template>
-    
+  
 </template>
 
 <script>
-    export default {}
+  export default {
+    async mounted () {
+      var id = Math.floor(Math.random() * Math.floor(20000))
+      this.$router.push('/comics/' + id)
+    }
+  }
 </script>
