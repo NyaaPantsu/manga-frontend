10c758bd927a19b916270bf4b3db890af67e79e6
PantsuDev <ewhal@pantsu.cat>
2018-02-05 22:21:47 +1000

    Refinement bug fixes

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,24 @@
           <div slot="header">Refine</div>
           <v-card>
             <v-card-text>
+                <v-select v-model="tag" label="Tags" :loading="loadingtags" dark :search-input.sync="tagsSearch" chips tags autocomplete
+            :items="tagResults"></v-select>
+          <v-select v-model="authors" label="Authors" :loading="loadingauthors" dark :search-input.sync="authorsSearch" chips tags autocomplete
+            :items="authorResults">
+          </v-select>
+          <v-select v-model="artists" label="Artists" :loading="loadingartists" dark :search-input.sync="artistsSearch" chips tags autocomplete
+            :items="artistResults"></v-select>
               <v-select v-model="aliases" label="Series Aliases" chips tags></v-select>
               <v-select v-model="statuses" label="Status" v-bind:items="this.status" item-value="text" name="status" required></v-select>
               <v-select v-model="typename" label="Type Name" v-bind:items="this.types" item-value="text" name="typename" required>
               </v-select>
               <v-select v-model="language" label="Language" v-bind:items="this.languages" autocomplete required></v-select>
               <v-select v-model="typedemonym" label="Type Demonym" v-bind:items="this.demonyms" item-value="text" name="typedemonym" required>
+              <v-checkbox label="Mature" v-model="mature" dark name="mature"></v-checkbox>
               </v-select>
+                        <v-btn @click="submit">
+            submit
+          </v-btn>
             </v-card-text>
           </v-card>
         </v-expansion-panel-content>
@@ -47,7 +58,25 @@
     },
     data: () => {
       return {
+        language: '',
+        aliases: '',
+        typename: '',
+        typedemonym: '',
+        statuses: '',
+        mature: false,
+        tagsSearch: null,
+        authorsSearch: null,
+        artistsSearch: null,
+        loadingtags: false,
+        loadingauthors: false,
+        loadingartists: false,
         chapters: [],
+        tag: [],
+        tagResults: [],
+        authorResults: [],
+        artistResults: [],
+        artists: [],
+        authors: [],
         items: [],
         count: 0,
         query: '',
@@ -179,11 +208,59 @@
       this.page = p
       this.tags = tags
     },
+    watch: {
+      tagsSearch (val) {
+        val && this.queryTags(val)
+      },
+      authorsSearch (val) {
+        val && this.queryAuthors(val)
+      },
+      artistsSearch (val) {
+        val && this.queryArtists(val)
+      }
+    },
     methods: {
+      submit: function (val) {
+
+      },
+      queryTags (v) {
+        this.loadingtags = true
+        // Simulated ajax query
+        setTimeout(() => {
+          this.$axios.$get('/tags?query=Name__icontains:' + v).then((response) => {
+            this.tagResults = response['response'].map(function (item) {
+              return item.Name
+            })
+            this.loadingtags = false
+          })
+        }, 500)
+      },
+      queryAuthors (v) {
+        this.loadingauthors = true
+        // Simulated ajax query
+        setTimeout(() => {
+          this.$axios.$get('/tags?query=NameSpace:author,Name__icontains:' + v).then((response) => {
+            this.authorResults = response['response'].map(function (item) {
+              return item.Name
+            })
+            this.loadingauthors = false
+          })
+        }, 500)
+      },
+      queryArtists (v) {
+        this.loadingartists = true
+        // Simulated ajax query
+        setTimeout(() => {
+          this.$axios.$get('/tags?query=NameSpace:artist,Name__icontains:' + v).then((response) => {
+            this.artistResults = response['response'].map(function (item) {
+              return item.Name
+            })
+            this.loadingartists = false
+          })
+        }, 500)
+      },
       click: function (val) {
-        console.log(this.$route.fullPath)
-        var temp = this.$route.fullPath.replace(/\/\?(p=\d)?/g, '')
-        console.log(temp)
+        var temp = this.$route.fullPath.replace(/\/\?(p=\d+)?/g, '')
         if (temp === '/') {
           this.$router.push('?p=' + val)
         } else {
